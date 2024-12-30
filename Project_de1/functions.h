16dc360d6f6dcffebf3c9abfceaeb1efab9a6bf0
letrungchien <letrungchien195@gmail.com>
2024-12-30 15:28:45 +0700

    Add files via upload

---
+++ Project_de1/functions.h
@@ -0,0 +1,14 @@
+#include "datatypes.h"
+#include<stdio.h>
+
+  void startTheProgram();
+  void menuAdmin();
+  void studentManagement();
+  void invalidSelection();
+  void endOfTheProgram(); 
+  void inputStudentInfo(Student students[], int *n, int maxSize);
+  void printStudents(Student students[], int n);
+  void addStudent(Student students[], int *n, int maxSize);
+  void searchStudentById(students, n);
+  
+  void deleteStudent(students,n);
